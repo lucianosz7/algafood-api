4f1e1bbf8a84b5efd6c78ec5cafef9bbf818a068
Luciano Santos <luciano_z7@hotmail.com>
2020-05-04 21:26:57 -0300

    Definindo beans com outras anotacoes

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -3,16 +3,32 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
-@Component
 public class NotificadorEmail implements Notificador {
 
-    public NotificadorEmail() {
+    private boolean caixaAlta;
+    private String hostServidorSmtp;
+
+    public NotificadorEmail(String hostServidorSmtp) {
+        this.hostServidorSmtp = hostServidorSmtp;
         System.out.println("NotificadorEmail");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        System.out.printf("Notificando %s através do e-mail %s: %s\n",
-                cliente.getNome(), cliente.getEmail(), mensagem);
+        if(this.caixaAlta){
+            mensagem = mensagem.toUpperCase();
+        }
+        System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), hostServidorSmtp, mensagem);
     }
+
+    public boolean isCaixaAlta() {
+        return caixaAlta;
+    }
+
+    public void setCaixaAlta(boolean caixaAlta) {
+        this.caixaAlta = caixaAlta;
+    }
+
+
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -2,9 +2,7 @@ package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AtivacaoClienteService {
 
     private Notificador notificador;
