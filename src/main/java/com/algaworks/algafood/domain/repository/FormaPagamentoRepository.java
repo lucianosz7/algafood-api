1b460ac205aa159c76dd1a7706c359553e252f94
Luciano Santos <F941008@CORP.CAIXA.GOV.BR>
2020-07-17 14:53:35 -0300

    Finalizando módulo JPA

---
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,8 @@
+package com.algaworks.algafood.domain.repository;
+
+public interface FormaPagamento {
+    List<FormaPagamento> listar();
+    FormaPagamento buscar(Long id);
+    FormaPagamento salvar(FormaPagamento formaPagamento);
+    void remover(FormaPagamento formaPagamento);
+}
